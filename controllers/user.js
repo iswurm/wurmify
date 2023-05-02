b0c9477c8992dfc68c4f723b40c6f7468e33b000
Ignacio <ignacio.sanchez@campusfp.es>
2023-05-02 21:51:57 +0200

    [FEATURE] Adición función Login

--- controllers/user.js
+++ controllers/user.js
@@ -4,54 +4,89 @@ var User = require('../models/user')
 const mongoose = require('mongoose');
 const usuarios = mongoose.model('User');
 
-async function pruebas(req, res){
-    res.status(200).send({message: "Controlador OK"});
+async function pruebas(req, res) {
+  res.status(200).send({ message: "Controlador OK" });
 }
 
-async function saveUser(req, res){
-    var user = new User();
-    var params = req.body;
+async function saveUser(req, res) {
+  var user = new User();
+  var params = req.body;
 
-    user.name = params.name;
-    user.surname = params.surname;
-    user.email = params.email;
-    user.role = 'ROLE_USER';
-    user.image = 'null';
-    
-    if(params.password){
-        var hash =  bcrypt.hashSync(params.password, 10);
-        user.password = hash;
-            if(user.name != null && user.surname != null && user.email != null){
-                //save
-                try {
-                    await new usuarios(user).save();
-                    return res.send({
-                        status: user
-                    });
-                  } catch (error) {
-                    return res.status(400).send({
-                      status: 'failure' + error
-                    });
-                  }
-            }else{
-                res.status(200).send({message: 'Rellene todos los campos'});
-            }
-        
-    }else{
-        res.status(200).send({message: 'Introduzca pass'});
+  user.name = params.name;
+  user.surname = params.surname;
+  user.email = params.email;
+  user.role = 'ROLE_USER';
+  user.image = 'null';
+
+  if (params.password) {
+    var hash = bcrypt.hashSync(params.password, 10);
+    user.password = hash;
+    if (user.name != null && user.surname != null && user.email != null) {
+      //save
+      try {
+        await new usuarios(user).save();
+        return res.send({
+          status: user
+        });
+      } catch (error) {
+        return res.status(400).send({
+          status: 'failure' + error
+        });
+      }
+    } else {
+      res.status(200).send({ message: 'Rellene todos los campos' });
     }
+
+  } else {
+    res.status(200).send({ message: 'Introduzca pass' });
+  }
 
 }
 
 async function findAll(req, res) {
-    try {
-      const users = await usuarios.find({});
-      return res.send(users && users.length ? users : []);
-    } catch (error) {
-      return res.status(400).send({
-        status: 'failure'
-      });
-    }
+  try {
+    const users = await usuarios.find({});
+    return res.send(users && users.length ? users : []);
+  } catch (error) {
+    return res.status(400).send({
+      status: 'failure'
+    });
+  }
 }
 
-module.exports = {pruebas, saveUser, findAll};
+async function loginUser(req, res) {
+  try {
+    const usuario = req.body;
+    const usuarioLeido = await usuarios.findOne({ 'email': usuario.email });
+    if (usuarioLeido) {
+      console.log(usuarioLeido);
+      if (!isValidPassword(usuarioLeido, usuario.password)) {
+        console.log('Invalid Password');
+      } else {
+        console.log(usuario.password);
+        
+        return res.status(200).send({
+          status: usuarioLeido
+        })
+      }
+    } else {
+      return res.send({ status: 'not found' });
+    }
+  } catch (error) {
+    return res.status(400).send({
+      status: 'failure'
+    });
+  }
+}
+
+function isValidPassword(user, password) {
+  var result = bcrypt.compareSync(password, user.password);
+  if (result) {
+    console.log("Password correct");
+  } else {
+    console.log("Password wrong");
+  }
+  return result;
+}
+
+module.exports = { pruebas, saveUser, findAll, loginUser };
--- routes/user.js
+++ routes/user.js
@@ -8,5 +8,6 @@ var router = express.Router();
 router.get('/controlador', UserController.pruebas);
 router.post('/register', UserController.saveUser);
 router.get('/usuarios', UserController.findAll);
+router.post('/login', UserController.loginUser);
 
 module.exports = router;
