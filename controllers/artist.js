fee6e389c2a4e6a1d06c593ae7dbabe7be5a6c9a
Ignacio Sánchez <ignacio.sanchez@sisnet360.com>
2023-05-03 13:42:07 +0200

    Artistas listado paginado y update

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,5 +1,12 @@
 'use strict'
-var Artist = require('../models/artist')
+
+var path = require('path');
+var fs = require('fs');
+var mongoosePagination = require('mongoose-pagination');
+
+var Artist = require('../models/artist');
+var Album = require('../models/album');
+var Song = require('../models/song');
 const mongoose = require('mongoose');
 const artistas = mongoose.model('Artist');
 
@@ -14,7 +21,7 @@ async function saveArtist(req, res) {
     artist.name = params.name;
     artist.description = params.description;
     artist.image = 'null';
-    
+
     if (artist.name != null && artist.description != null) {
         //save
         try {
@@ -35,8 +42,8 @@ async function saveArtist(req, res) {
 
 async function findAll(req, res) {
     try {
-        const artists = await artists.find({});
-        return res.send(artist && artist.length ? artist : []);
+        const artists = await Artist.find({});
+        return res.send(artists && artists.length ? artists : []);
     } catch (error) {
         return res.status(400).send({
             status: 'failure'
@@ -44,5 +51,59 @@ async function findAll(req, res) {
     }
 }
 
+async function getArtist(req, res){
+    var artistId = req.params.id;
+    try{
+        const artist = await Artist.findById({artistId});
+        if(!artist){
+            return res.status(404).send("El artista no existe");    
+        }else{
+            return res.status(200).send({artist});
+        }
+        
+    }catch(error){
+        return res.status(400).send({
+            status: 'failure'
+        });
+    }
+}
 
-module.exports = { pruebas, saveArtist, findAll };
+async function getArtistPaginado(req, res){
+    var page = req.params.page;
+    var itemsPerPage = 3;
+    try{
+        const artists = await Artist.find().sort('name').paginate(page, itemsPerPage);
+        if(!artists){
+            return res.status(404).send("No existen artistas"); 
+        }else{
+            return res.status(200).send({
+                pages: total,
+                artists: artists
+            });
+        }
+    }catch(error){
+        return res.status(400).send({
+            status: 'failure'
+        });
+    }
+    
+}
+
+async function updateArtist(req, res){
+    var artistId = req.params.id;
+    var update = req.body;
+  
+    try {
+      const artist = await artistas.findByIdAndUpdate(artistId, update);
+      return res.send({
+        artist: artist
+      });
+    } catch (error) {
+      return res.status(400).send({
+        status: 'No se ha podido modificar'
+      });
+    }
+  }
+
+
+module.exports = { pruebas, saveArtist, findAll, getArtist, getArtistPaginado, updateArtist };
--- routes/artist.js
+++ routes/artist.js
@@ -4,7 +4,12 @@ var express = require('express');
 var ArtistController = require('../controllers/artist');
 
 var router = express.Router();
+var md_auth = require('../middlewares/authenticated');
 
 router.get('/artists', ArtistController.findAll);
+router.get('/artist/:id', ArtistController.getArtist);
+router.get('/artist/:page', ArtistController.getArtistPaginado);
+router.post('/artist', ArtistController.saveArtist);
+router.put('/update-artist/:id', md_auth.ensureAuth, ArtistController.updateArtist);
 
 module.exports = router;
