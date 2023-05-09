aa6830aff88c8b70510816ac81dd0b8718e20cd0
Ignacio <ignacio.sanchez@campusfp.es>
2023-05-09 23:07:40 +0200

    [FEATURE] Modelos angular

--- app-wurmify/src/app/app.component.ts
+++ app-wurmify/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app-wurmify';
+  title = 'Wurmify';
+
+  public user: User;
+  public identity: any;
+  public token: any;
+
+  constructor(){
+    this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+  }
+
 }
