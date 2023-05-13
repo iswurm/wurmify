fb39fb6e17c6812b01ca09b0a457e575b7d46760
Ignacio <ignacio.sanchez@campusfp.es>
2023-05-13 21:25:36 +0200

    [FEATURE] Login funcional angular

--- app-wurmify/src/app/app.component.ts
+++ app-wurmify/src/app/app.component.ts
@@ -1,20 +1,32 @@
 import { Component } from '@angular/core';
 import { User } from './models/user';
+import { UserServiceService } from './services/user-service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [UserServiceService]
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Wurmify';
 
   public user: User;
   public identity: any;
   public token: any;
 
-  constructor(){
+  constructor(private _userService:UserServiceService){
     this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+  }
+  
+  ngOnInit(){
+  }
+
+  onSumbit(){
+    console.log(this.user);
+    this._userService.login(this.user).subscribe((data) => {
+      console.log(data);
+    });
   }
 
 }
