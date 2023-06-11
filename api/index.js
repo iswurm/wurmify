88651c2c166070d59c001720bcfa66684dda36e1
Ignacio <ignacio.sanchez@campusfp.es>
2023-06-11 23:47:27 +0200

    [FIX] Test para  render.com

--- api/index.js
+++ api/index.js
@@ -5,24 +5,24 @@ var app = require('./app.js');
 var port = process.env.PORT || 3977;
 
 const options = {
-    autoIndex: false, // Don't build indexes
-    maxPoolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-  };
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4 // Use IPv4, skip trying IPv6
+};
 
 const uri = process.env.MONGODB_URI;
 
-let connectWithRetry= function() {
-    mongoose.Promise = global.Promise;
-    return mongoose.connect(uri, options).then(() => { 
-        app.listen(port, function(){
-          console.log("Está funcionando");
-        })
-      },
-      err => { console.log("Error de conexión.") }
-    );
-  };
+let connectWithRetry = function () {
+  mongoose.Promise = global.Promise;
+  return mongoose.connect(uri, options).then(() => {
+    app.listen(port, function () {
+      console.log("Está funcionando");
+    })
+  },
+    err => { console.log("Error de conexión." + err) }
+  );
+};
 
-  connectWithRetry();
+connectWithRetry();
